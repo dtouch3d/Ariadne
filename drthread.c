95e1962b000c5d69729060dbb0e8df4e5ca34200
Dimitrios Tatsis <dtouch3d@gmail.com>
2014-07-26 19:10:08 +0300

    added module load handler

--- drthread.c
+++ drthread.c
@@ -12,9 +12,17 @@ event_thread_exit(void *drcontext)
     dr_printf("[+] Thread exit event\n");
 }
 
+
+static void
+event_module_load(void *drcontext, const module_data_t *info, bool loaded)
+{
+    dr_printf("[+] Loaded module %s\n", dr_module_preferred_name(info));
+}
+
 DR_EXPORT void
 dr_init(client_id_t id)
 {
     dr_register_thread_init_event(event_thread_init);
     dr_register_thread_exit_event(event_thread_exit);
+    dr_register_module_load_event(event_module_load);
 }
