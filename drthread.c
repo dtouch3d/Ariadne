299577ab76a82ae3fe0650392b67e40ab2e0ed98
Dimitrios Tatsis <dtouch3d@gmail.com>
2014-07-26 21:52:29 +0300

    Code to find func addresses in module

--- drthread.c
+++ drthread.c
@@ -1,4 +1,11 @@
 #include "dr_api.h"
+#include <string.h>
+
+#define SIZE(A) sizeof(A)/sizeof(A[0])
+
+const char* const threadlib[] = { "libpthread" };
+
+const char* const threadfuncs[][10] = { {"pthread_mutex_lock", "pthread_create"} };
 
 static void
 event_thread_init(void *drcontext)
@@ -12,11 +19,26 @@ event_thread_exit(void *drcontext)
     dr_printf("[+] Thread #%d exit\n", dr_get_thread_id(drcontext));
 }
 
-
 static void
 event_module_load(void *drcontext, const module_data_t *info, bool loaded)
 {
-    dr_printf("[+] Loaded module %s\n", dr_module_preferred_name(info));
+    int i,j;
+    const char* lib = dr_module_preferred_name(info);
+    module_handle_t lib_handle = info->handle;
+
+    /* We check if a library we want to instrument is loaded */
+    for(i=0; i<SIZE(threadlib); i++)
+    {
+        if(strstr(lib, threadlib[i]) != NULL)
+        {
+            dr_printf("[+] Loaded %s library\n", lib);
+            for(j=0; j<SIZE(threadfuncs[i]) && threadfuncs[i][j] != NULL; j++)
+            {
+                void* addr = dr_get_proc_address(lib_handle, threadfuncs[i][j]);
+                dr_printf("[+] Found %s @ %p\n", threadfuncs[i][j], addr);
+            }
+        }
+    }
 }
 
 DR_EXPORT void
