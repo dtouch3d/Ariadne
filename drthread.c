402c0fe00786a0152de523dcc770eb6e4709e30a
Dimitrios Tatsis <dtouch3d@gmail.com>
2014-07-26 16:12:55 +0300

    Added thraed init/exit hooks

--- drthread.c
+++ drthread.c
@@ -1,7 +1,22 @@
 #include "dr_api.h"
 
+static void
+event_thread_init(void *drcontext)
+{
+    static unsigned int tid = 0;
+    tid++;
+    dr_printf("[+] Created thread #%d\n", tid);
+}
+
+static void
+event_thread_exit(void *drcontext)
+{
+    dr_printf("[+] Thread exit event\n");
+}
+
 DR_EXPORT void
 dr_init(client_id_t id)
 {
-    /*empty*/
+    dr_register_thread_init_event(event_thread_init);
+    dr_register_thread_exit_event(event_thread_exit);
 }
