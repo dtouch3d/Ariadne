a3eb665a0502951c47f51f434b64cd932f0e250d
Dimitrios Tatsis <dtouch3d@gmail.com>
2014-07-28 20:21:39 +0300

    Added drsym init and exit, trivial name change

--- drthread.c
+++ drthread.c
@@ -1,5 +1,6 @@
 #include "dr_api.h"
 #include "drwrap.h"
+#include "drsyms.h"
 #include <string.h>
 
 #include "drthread.h"
@@ -8,19 +9,19 @@ static void
 event_module_load(void *drcontext, const module_data_t *info, bool loaded)
 {
     int i,j;
-    const char* lib = dr_module_preferred_name(info);
-    module_handle_t lib_handle = info->handle;
+    const char* modname = dr_module_preferred_name(info);
+    module_handle_t modhandle = info->handle;
 
     /* We check if a library we want to instrument is loaded */
     for(i=0; i<SIZE(threadlib); i++)
     {
-        if(strstr(lib, threadlib[i]) != NULL)
+        if(strstr(modname, threadlib[i]) != NULL)
         {
-            dr_printf("[+] Loaded %s library\n", lib);
+            dr_printf("[+] Loaded %s module\n", modname);
             for(j=0; j<SIZE(symtab); j++)
             {
                 symtab_t func = symtab[j];
-                void* addr = dr_get_proc_address(lib_handle, func.name);
+                void* addr = dr_get_proc_address(modhandle, func.name);
                 dr_printf("[+] Found %s @ %p\n", func.name, addr);
                 void (*funcp)() = findfunc(func.name);
                 dr_printf("%s handler is at %p\n", func.name, funcp);
@@ -35,12 +36,14 @@ static void
 event_exit(void)
 {
     drwrap_exit();
+    drsym_exit();
 }
 
 DR_EXPORT void
 dr_init(client_id_t id)
 {
     drwrap_init();
+    drsym_init(0);
     dr_register_module_load_event(event_module_load);
     dr_register_exit_event(event_exit);
 
