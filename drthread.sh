a5bb7e3e5697c1655753e65e673a12ab80e6ff49
Dimitrios Tatsis <dtouch3d@gmail.com>
2014-07-30 01:27:45 +0300

    Added dr symlink at build dir, tests don't work

---
+++ drthread.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Variables are replaces with their proper values at
+# configure time by cmake.
+
+DR_OPS=
+
+${PROJECT_BINARY_DIR}/dynamorio/bin@ARCH@/drrun -c @DRTHREAD_PATH@ -- $@
--- tests/runtest.cmake
+++ tests/runtest.cmake
@@ -6,9 +6,16 @@
 message(STATUS "${TEST_PROG} ::: ${SRCDIR}")
 
 # TODO: Args ?
-execute_process(COMMAND ${TEST_PROG}
+execute_process(COMMAND ../drthread.sh ${TEST_PROG}
     RESULT_VARIABLE result
     OUTPUT_VARIABLE output
     ERROR_VARIABLE err)
 
-message(STATUS "${err}")
+file(READ "${TEST_PROG}.expect" expect)
+
+#message(STATUS "${expect}")
+#message(STATUS "::: ${output}")
+
+if(NOT "${output}" MATCHES "${expect}")
+    message(FATAL_ERROR "Test ${TEST_PROG} Failed!")
+endif()
